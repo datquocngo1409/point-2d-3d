118fd7a67f08609f4e1fe739a7bba72f4dbab5e9
NgoQuocDat <shaco14091999@gmail.com>
2018-10-27 11:12:47 +0700

    add

---
+++ [OOP]Point/src/oop/point/OOPPoint.java
@@ -0,0 +1,13 @@
+package oop.point;
+
+public class OOPPoint {
+
+    public static void main(String[] args) {
+        Point2D p2 = new  Point2D(5, 7);
+        Point3D p3 = new Point3D(3, 7, 8);
+        
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+    }
+    
+}
